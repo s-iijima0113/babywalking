f56103616f0b402cb03eecd9c522f29f00d18dae
s-iijima0113 <everyth1ng.n1ce.St01@gmail.com>
2025-09-07 15:55:29 +0900

    地図のポップアップ情報追加

--- internal/db/db.go
+++ internal/db/db.go
@@ -104,12 +104,20 @@ func AddDb(edited [][]string) {
 
 // MapBoxに渡すAPI作成
 type Facility struct {
-	ID      int     `json:"id"`
-	Toilet  string  `json:"toilet"`
-	Nursing string  `json:"nursing"`
-	Lat     float64 `json:"lat"`
-	Lon     float64 `json:"lon"`
-	Name    string  `json:"name"`
+	ID              int     `json:"id"`
+	Toilet          string  `json:"toilet"`
+	Nursing         string  `json:"nursing"`
+	Lat             float64 `json:"lat"`
+	Lng             float64 `json:"lng"`
+	Name            string  `json:"name"`
+	Others          string  `json:"others"`
+	Features        string  `json:"features"`
+	Postcode        string  `json:"postcode"`
+	Address         string  `json:"address"`
+	PhoneNumber     string  `json:"phone_number"`
+	OpeningHours    string  `json:"opening_hours"`
+	RegularHolidays string  `json:"regular_holidays"`
+	Website         string  `json:"website"`
 }
 
 func FacilityAPI() {
@@ -122,7 +130,7 @@ func FacilityAPI() {
 	defer db.Close()
 
 	http.HandleFunc("/api/facilities", func(w http.ResponseWriter, r *http.Request) {
-		rows, err := db.Query("SELECT id, toilet, nursing, lat, lon, name FROM baby_facilities")
+		rows, err := db.Query("SELECT id, toilet, nursing, lat, lng, name, others, features, postcode, address, phone_number, opening_hours, regular_holidays, website FROM baby_facilities")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -132,7 +140,9 @@ func FacilityAPI() {
 		var facilities []Facility
 		for rows.Next() {
 			var f Facility
-			if err := rows.Scan(&f.ID, &f.Toilet, &f.Nursing, &f.Lat, &f.Lon, &f.Name); err != nil {
+			if err := rows.Scan(&f.ID, &f.Toilet, &f.Nursing, &f.Lat, &f.Lng, &f.Name,
+				&f.Others, &f.Features, &f.Postcode, &f.Address, &f.PhoneNumber, &f.OpeningHours,
+				&f.RegularHolidays, &f.Website); err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
